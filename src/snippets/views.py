eb3558d23941cab56d4c3c0536b7a872ff079672
“vivek0079” <vivek.vivek0079@gmail.com>
2018-05-30 07:49:06 +0530

    Created the template

--- src/snippets/views.py
+++ src/snippets/views.py
@@ -7,16 +7,19 @@ from .serializers import SnippetSerializer
 
 @api_view(['GET', 'POST'])
 def snippet_list_or_create(request, format=None):
+    template_name = 'api.html'
     if request.method == 'GET':
         snippet = Snippet.objects.all()
         serializer = SnippetSerializer(snippet, many=True)
+        if request.accepted_renderer.format == 'html':
+            return Response(serializer.data, template_name=template_name)
         return Response(serializer.data)
-
+        
     elif request.method == 'POST':
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, template_name=template_name)
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
